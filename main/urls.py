a8e014db3bac3c58d0cfe527805adce3825a346b
irybtsov <rybtsov_i_v@mail.ru>
2021-03-05 19:33:00 +0300

    Added Profile, Search pages. Also base2 template for future use

--- main/urls.py
+++ main/urls.py
@@ -4,6 +4,8 @@ from . import views
 app_name = 'main'
 urlpatterns = [
     path('', views.index, name='home'),
-    path('about', views.about, name='about'),
     path('catalogue', views.catalogue, name='catalogue'),
+    path('search', views.search, name='search'),
+    path('profile', views.profile, name='profile'),
+    path('about', views.about, name='about'),
 ]
--- main/views.py
+++ main/views.py
@@ -9,5 +9,11 @@ def catalogue(request):
     recipes = Recipe.objects.order_by('id')[:10]
     return render(request, 'main/catalogue.html', {'title': 'Catalogue', 'recipes': recipes})
 
+def search(request):
+    return render(request, 'main/search.html', {'title': 'Search'})
+
+def profile(request):
+    return render(request, 'main/profile.html', {'title': 'Profile'})
+
 def about(request):
     return render(request, 'main/about.html', {'title': 'About'})
