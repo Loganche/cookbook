dc1fb8a8544f520cd680482455883a7d9e3c4405
irybtsov <rybtsov_i_v@mail.ru>
2021-03-05 18:34:48 +0300

    Main Page, About Page, model Recipe

---
+++ main/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+class Recipe(models.Model):
+    title = models.CharField('Recipe Name', max_length=50)
+    description = models.TextField('Description')
+
+    def __str__(self):
+        return self.title
